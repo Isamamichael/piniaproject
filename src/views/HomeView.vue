6f6c36b1b7cb934141b163a5bf277242b8e20348
Isamamichael <isamainalegwu20@gmail.com>
2023-03-27 10:13:53 +0100

    Praise pinia project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css
+">
+  </head>
+  <body class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col md-6">
+           <img alt="download" src="../assets/images/download.png" width="550" height="300" />
+      <p> welcome to catherinal Heights Academy</p>
+      <p>A place where great leaders are trained</p>
+        </div>
+      </div>
+    </div>
+  
+  </body>
+  </html>
+</template>
+<style>
+.container{
+  background-color: rgb(4, 4, 85);
+  color: white;
+}
+</style>
